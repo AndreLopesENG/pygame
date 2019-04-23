715a2b3ab1b08977e32f7b04f4d620b7d03eb80b
AndreLopesENG <lopesandre450@gmail.com>
2019-04-23 16:54:49 -0300

    cria a movimentação da nave

--- asteroids-base.py
+++ asteroids-base.py
@@ -32,7 +32,16 @@ class Player(pygame.sprite.Sprite):
         self.rect=self.image.get_rect()
         self.rect.centerx=WIDTH/2
         self.rect.bottom=HEIGHT-10
-
+        self.speedx=0
+    
+    def update(self):
+        self.rect.x += self.speedx
+        if self.rect.right>WIDTH:
+            self.rect.right=WIDTH
+        if self.rect.left<0:
+            self.rect.left=0
+            
+class 
 # Inicialização do Pygame.
 pygame.init()
 pygame.mixer.init()
@@ -50,6 +59,10 @@ clock = pygame.time.Clock()
 background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
 background_rect = background.get_rect()
 
+# Cria um grupo de sprites e adiciona a nave
+player=Player()
+all_sprites=pygame.sprite.Group()
+all_sprites.add(player)
 # Comando para evitar travamentos.
 try:
     
@@ -66,7 +79,17 @@ try:
             # Verifica se foi fechado
             if event.type == pygame.QUIT:
                 running = False
-    
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.speedx=-8
+                if event.key==pygame.K_RIGHT:
+                    player.speedx=8
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    player.speedx=0
+                if event.key == pygame.K_RIGHT:
+                    player.speedx=0
+        all_sprites.update()
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
         screen.blit(background, background_rect)
